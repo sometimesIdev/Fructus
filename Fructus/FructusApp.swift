02cf14918a6797a9357f0aec375d91eb7cced692
Pat Butler <15131675+Beefycoder@users.noreply.github.com>
2022-04-07 17:02:32 -0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Pat Butler on 2022-04-07.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
